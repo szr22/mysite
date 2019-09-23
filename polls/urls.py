d5e03b744d9247d3bc0759c87f4865cc84c4e617
Zhenrong <shizhenrong1987@hotmail.com>
2019-09-23 12:24:49 -0400

    using 404 shortcut, and remove hardcode url

--- polls/urls.py
+++ polls/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # added the word 'specifics'
+    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 ]
